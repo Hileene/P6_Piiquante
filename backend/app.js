abbd7ac4974dc37597f79c760bc829e35ed4ae9e
Hileene <linebeh@gmail.com>
2023-03-07 00:44:39 +0100

    Connection à la base de donnée

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
-//IMPORTATION FRAMEWORK
+//IMPORTATION FRAMEWORK/BASE DE DONNÉE
 const express = require('express');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 
 const app = express();
+
+//CONNECTION DE MON API A MON CLUSTER MONGODB
+mongoose.connect('mongodb+srv://aline_bb:MdJxgNYGVWdDY6n8@cluster0.jj6ina4.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
