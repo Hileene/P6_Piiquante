283f041d9ea204bb4d0f6f0a1f30fa2ea2fa4739
Hileene <linebeh@gmail.com>
2023-03-07 01:17:28 +0100

    Création du modèle user

---
+++ backend/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// On rajoute mongoose-unique-validator comme plugin à notre schéma
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+    email: { type: String, required: true, unique: true }, //pour ne pas s'inscrire plusieurs fois avec la même adresse email
+    password: { type: String, required: true },
+});
+
+//On va appliquer le validator au schéma avant d'en faire un model
+// on va appeler la méthode plugin
+userSchema.plugin(uniqueValidator);
+
+// On exporte ce schéma sous forme de model
+module.exports = mongoose.model('User', userSchema);
