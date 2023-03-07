6880ecaf3f9081a394c837f2282e5ec6281b9cb1
Hileene <linebeh@gmail.com>
2023-03-07 01:29:42 +0100

    Ajout méthodes signup et login

---
+++ backend/controllers/user.js
@@ -0,0 +1,10 @@
+// Cette fonction est pour l'enregistrement de nouveaux utilisateurs
+exports.signup = ( req, res, next ) => {
+
+}
+
+
+// Cette fonction est pour connecter des utilisateurs existants
+exports.login = ( req, res, next ) => {
+    
+}
