ad14fee9af5af74ab6638e3a16e49db52142a70f
Hileene <linebeh@gmail.com>
2023-03-07 01:20:59 +0100

    Ajout middelware d'analyse du corps requête

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
-const app = express();
-
 //CONNECTION DE MON API A MON CLUSTER MONGODB
 mongoose.connect('mongodb+srv://aline_bb:MdJxgNYGVWdDY6n8@cluster0.jj6ina4.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
+{ useNewUrlParser: true,
     useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+    
+const app = express();
 
+// MIDDLEWARE GÉNÉRALE POUR TOUTES LES ROUTES
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+//Pour analyser le corps de la requête
+  app.use(express.json());
+  
 
 
 
