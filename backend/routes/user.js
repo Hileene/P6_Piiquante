313f6e516544e65d7eb13c0304dd4449e22e4811
Hileene <linebeh@gmail.com>
2023-03-10 11:11:19 +0100

    Modif ordre des constantes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,8 @@
 //CONFIGURATION DES ROUTES
 const express = require('express');
-const userCtrl = require('../controllers/user')
-
 //CRÉATION DU ROUTER AVEC LA FONCTION ROUTER D'EXPRESS
 const router = express.Router();
+const userCtrl = require('../controllers/user')
 
 // ICI ON UTILISE DES ROUTES POST CAR LE FRONTEND VA AUSSI ENVOYER DES INFOS(EMAIL ET PASSWORD)
 router.post('/signup', userCtrl.signup);
