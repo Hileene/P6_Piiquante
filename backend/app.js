a5adf792f2d72552032caaaf2b0ceb05489769ba
Hileene <92981899+Hileene@users.noreply.github.com>
2024-01-05 12:12:52 +0100

    Suppression du header CORS pour images

--- backend/app.js
+++ backend/app.js
@@ -29,8 +29,6 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  // Header pour acepter les requêtes images
-  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   next();
 });
 
